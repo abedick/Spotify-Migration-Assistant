7e82e4804ff36a0c16ec44abb0c9479c547381df
Abe Dick <abedick@ku.edu>
2018-04-21 15:14:16 -0500

    Updating configuration documentation

--- config/config.go
+++ config/config.go
@@ -1,3 +1,18 @@
+/*
+ *
+ * Package Config
+ *
+ * Desc		: An easy configuration system that stores all neccessary global
+ *			  program variables in a hash table that can quickly be updated,
+ *			  read, and saved to file.
+ *
+ *			  Configuration files are not meant to be shared and are in no way
+ *			  encrypted.
+ *
+ * Author	: Abe Dick <abedick8213@gmail.com>
+ * Date		: April, May 2018
+ */
+
 package config
 
 import (
@@ -7,59 +22,89 @@ import (
 	"strings"
 )
 
-var FILE string
-var ALIAS []string
-var MAP []string
-
+/*
+ * Load Configuration
+ *
+ * * PUBLIC METHOD * *
+ *
+ * Desc : Attempts to load into a hash table configuration settings read in
+ *		  from a settings file with the name 'file'. If this can be done, the
+ *		  data is returned, else a setup procedure is ran to collect neccessary
+ *		  configuration details from the user.
+ *
+ */
 func Load_config(file string, param_alias []string, param_map []string) map[string]string {
 
-	FILE = file
-	ALIAS = param_alias
-	MAP = param_map
-
-	settings, err := read_config()
+	settings, err := read_config(file, param_alias, param_map)
 
 	if err != nil {
-		settings = setup()
+		settings = setup(file, param_alias, param_map)
 	}
 
 	return settings
 }
 
-func Update_config(config map[string]string, param_name string, param_value string) map[string]string {
-	config[param_name] = param_value
+/*
+ * Update Configuration
+ *
+ * * PUBLIC METHOD * *
+ *
+ * Desc : Adds a variable and value to the configuration and saves the updates
+ * 		  to file.
+ *
+ */
+func Update_config(file string, config map[string]string, param_name string, param_value string) map[string]string {
 
-	save_config(config)
+	config[param_name] = param_value
+	save_config(file, config)
 
 	return config
 }
 
-func setup() map[string]string {
+/*
+ * Setup
+ *
+ * * PRIVATE METHOD * *
+ *
+ * Desc : Gathers all parameters specified in param map from the user.
+ *
+ */
+func setup(file string, param_alias []string, param_map []string) map[string]string {
 
 	tmp_config := make(map[string]string)
 
-	for i, _ := range ALIAS {
+	for i, _ := range param_alias {
 		var value string
-		fmt.Print(ALIAS[i], ": ")
+		fmt.Print(param_alias[i], ": ")
 		fmt.Scanln(&value)
-		tmp_config[MAP[i]] = value
+		tmp_config[param_map[i]] = value
 	}
 
-	save_config(tmp_config)
+	save_config(file, tmp_config)
 
 	return tmp_config
 }
 
-func save_config(conf map[string]string) {
+/*
+ * Save Configuration
+ *
+ * * PRIVATE METHOD * *
+ *
+ * Desc : Saves all parameters stored in the hash table in plain text.
+ *
+ * Future update may contain encryption for saved parameters.
+ *
+ */
+func save_config(file string, conf map[string]string) {
 
-	file, err := os.Create(FILE)
+	write_file, err := os.Create(file)
 
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	defer write_file.Close()
 
-	writer := bufio.NewWriter(file)
+	writer := bufio.NewWriter(write_file)
 
 	for key, value := range conf {
 		line := strings.Join([]string{key, value}, "=")
@@ -68,17 +113,29 @@ func save_config(conf map[string]string) {
 	writer.Flush()
 }
 
-func read_config() (map[string]string, error) {
+/*
+ * Read Configuration
+ *
+ * * PRIVATE METHOD * *
+ *
+ * Desc : Attempts to load all configuration parameters from the specified save
+ *		  file and into a hash table. If a parameter is missing from the read
+ *		  in file but is present in the list of expected parameters, it is now
+ *		  gathered from the user. If a parameter is read and not an expected
+ *		  parameter it is ignored.
+ *
+ */
+func read_config(file string, param_alias []string, param_map []string) (map[string]string, error) {
 	conf := make(map[string]string)
 
-	file, err := os.Open(FILE)
+	read_file, err := os.Open(file)
 
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+	defer read_file.Close()
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(read_file)
 
 	for scanner.Scan() {
 		key_value := strings.Split(scanner.Text(), "=")
@@ -88,21 +145,21 @@ func read_config() (map[string]string, error) {
 	update := false
 
 	/* Make sure that all config variables needed were read */
-	for i, value := range MAP {
+	for i, value := range param_map {
 
 		if _, ok := conf[value]; !ok {
 
 			var value string
-			fmt.Print(ALIAS[i], ": ")
+			fmt.Print(param_alias[i], ": ")
 			fmt.Scanln(&value)
-			conf[MAP[i]] = value
+			conf[param_map[i]] = value
 
 			update = true
 		}
 	}
 
 	if update {
-		save_config(conf)
+		save_config(file, conf)
 	}
 
 	return conf, nil
